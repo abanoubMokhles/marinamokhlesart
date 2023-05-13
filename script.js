d168a18b1cbd166f9e3368f3249b0cbf96a21164
abanoub mokhles <mokhles.abanoub1@gmail.com>
2023-05-13 18:48:51 +0300

    finish project

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const btn = document.querySelector("#navigation-btn");
 btn.addEventListener("click", (evt) => {
   nav.classList.toggle("active");
 });
+document.querySelector(".header__list").addEventListener("click", (evt) => {
+  if (!evt.target.classList.contains("header__link")) return;
+  document.querySelector(".header__navigation").classList.remove("active");
+});
 
 // Implement smooth scrolling
 //==============================
@@ -31,6 +35,7 @@ const callback = (entries, observer) => {
   if (!entry.isIntersecting) return;
 
   entry.target.src = entry.target.dataset.src;
+
   entry.target.addEventListener("load", () => {
     entry.target.classList.remove("lazy-image");
   });
@@ -40,7 +45,7 @@ const callback = (entries, observer) => {
 const options = {
   root: null,
   threshold: 0,
-  rootMargin: "100px",
+  rootMargin: "-100px",
 };
 
 const observer = new IntersectionObserver(callback, options);
@@ -51,3 +56,123 @@ allImages.forEach((img) => {
 
 // Implement slider component
 //==============================
+const slider = function () {
+  let allSlides = document.querySelectorAll(".slider__slide");
+  let leftBtn = document.querySelector(".slider__btn--left");
+  let rightBtn = document.querySelector(".slider__btn--right");
+  let dotsContainer = document.querySelector(".slider__dots");
+
+  // Current Slide & slides length
+  let currentSlide = 0;
+  let slidesNumber = allSlides.length;
+
+  // Position Slides
+  const positionSlides = function (currentSlide) {
+    allSlides.forEach(function (slide, index) {
+      slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+    });
+  };
+
+  // Creating Dots
+  const createDots = function () {
+    allSlides.forEach((_, i) => {
+      dotsContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class='slider__dot' data-slide='${i}'></button>`
+      );
+    });
+  };
+
+  // Style Dots
+  const activateDot = function (currentS) {
+    // Remove the active class
+    document.querySelectorAll(".slider__dot").forEach((dot) => {
+      dot.classList.remove("slider__dot--active");
+    });
+
+    // add Active class to current slide's dot
+    document
+      .querySelector(`.slider__dot[data-slide="${currentS}"]`)
+      .classList.add("slider__dot--active");
+  };
+
+  // Initial Settings
+  const init = function () {
+    positionSlides(0);
+    createDots();
+    activateDot(0);
+  };
+  init();
+
+  // Next Slide
+  const nextSlide = function () {
+    if (currentSlide === slidesNumber - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    positionSlides(currentSlide);
+    activateDot(currentSlide);
+  };
+
+  // Prev Slide
+  const prevSlide = function () {
+    if (currentSlide <= 0) {
+      currentSlide = slidesNumber - 1;
+    } else {
+      currentSlide--;
+    }
+    positionSlides(currentSlide);
+    activateDot(currentSlide);
+  };
+
+  rightBtn.addEventListener("click", nextSlide);
+  leftBtn.addEventListener("click", prevSlide);
+
+  // listening to Keyboard
+  document.addEventListener("keydown", function (e) {
+    const key = e.key;
+    if (key === "ArrowRight") {
+      nextSlide();
+    } else if (key === "ArrowLeft") {
+      prevSlide();
+    }
+  });
+
+  // listening to dots clicks
+  dotsContainer.addEventListener("click", function (evt) {
+    if (evt.target.classList.contains("slider__dot")) {
+      // move to the slide
+      positionSlides(evt.target.dataset.slide);
+      activateDot(evt.target.dataset.slide);
+    }
+  });
+};
+
+slider();
+
+// Implement Reveal on scroll
+//==============================
+
+const revealCallback = (entries, observer) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
+
+const revealOptions = {
+  root: null,
+  threshold: 0.5,
+};
+const revealObserver = new IntersectionObserver(revealCallback, revealOptions);
+document.querySelectorAll(".section-hidden").forEach((section) => {
+  revealObserver.observe(section);
+});
+
+// Submitting the form
+//======================
+document.querySelector(".form").addEventListener("submit", (evt) => {
+  evt.preventDefault();
+});
